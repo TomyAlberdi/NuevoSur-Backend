9700d0345dd648f0b9967731a3cb85375276b5bd
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-07-17 20:12:30 -0300

    Advanced auth implementation

--- BackendService/src/main/java/com/example/backendservice/Security/JwtAuthenticationFilter.java
+++ BackendService/src/main/java/com/example/backendservice/Security/JwtAuthenticationFilter.java
@@ -8,12 +8,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     
@@ -30,11 +36,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (token != null && token.startsWith("Bearer ")) {
             try {
                 DecodedJWT jwt = verifier.verify(token.substring(7));
+    
+                // Extract roles from token
+                List<GrantedAuthority> authorities = jwt.getClaim("roles").asList(Map.class).stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.get("key").toString()))
+                        .collect(Collectors.toList());
+    
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        jwt.getSubject(), null, Collections.emptyList());
+                        jwt.getSubject(), null, authorities);
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (JWTVerificationException e) {
+                logger.error("JWT verification failed", e);
                 SecurityContextHolder.clearContext();
             }
         }
